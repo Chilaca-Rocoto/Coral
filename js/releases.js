6b8d638e5ea58433e121af66f0ef17b8da61a689
BazeJared <jaredtanbaze@gmail.com>
2026-02-02 17:32:00 -0800

    Update releases.js

--- js/releases.js
+++ js/releases.js
@@ -58,10 +58,6 @@ async function loadReleases() {
           : "";
         const date = formatDate(r.published_at);
         const url = r.html_url;
-
-        // const body = (r.body || "").trim();
-        // const snippet =
-        //   body.length > 220 ? escapeHtml(body.slice(0, 220)) + "…" : escapeHtml(body);
         const body = (r.body || "").trim();
 
         const snippet =
